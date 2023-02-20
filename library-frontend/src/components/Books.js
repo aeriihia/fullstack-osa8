46ad33d4fd8d5f9cc7008dd165de52387de5c679
aeriihia <89082133+aeriihia@users.noreply.github.com>
2023-02-21 00:24:30 +0200

    8.19

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Books = (props) => {
+  const [genre, setGenre] = useState('all genres')
+  const [genres, setGenres] = useState([])
+  const [books, setBooks] = useState(props.books)
+
+  useEffect(() => {
+    let allGenres = ['all genres']
+    for (let b of books) {
+      for (let g of b.genres) {
+        if (!allGenres.includes(g)) {
+          allGenres.push(g)
+        }
+      }
+    }
+    setGenres(allGenres)
+  }, [])
+
   if (!props.show) {
     return null
   }
 
-  const books = props.books
+  const selectGenre = (g) => {
+    setGenre(g)
+    if (g === 'all genres') {
+      setBooks(props.books)
+    } else {
+      setBooks(props.books.filter((b) => b.genres.includes(g)))
+    }
+  }
 
   return (
     <div>
       <h2>books</h2>
-
+      <div>in genre <b>{genre}</b></div>
       <table>
         <tbody>
           <tr>
@@ -27,6 +50,9 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <div>
+        {genres.map((g) => (<button key={g} onClick={() => selectGenre(g)}>{g}</button>))}
+      </div>
     </div>
   )
 }
